c610837dec44109fda6043f2f931eff515883ca6
Joel Jogy <joeljogy07@gmail.com>
2020-06-16 06:13:56 +0530

    Minor fixes

--- find_boundingboxes.py
+++ find_boundingboxes.py
@@ -17,10 +17,15 @@ if ("analyzeResult" in analysis):
     # Extract the recognized text, with bounding boxes.
     polygons = [(line["boundingBox"], line["text"])
                 for line in analysis["analyzeResult"]["readResults"][0]["lines"]]
-
+target_fields = ['Healthcare Organization Name:','Provider Name:','NPI #:','Other(s)','Location Address:','City, State, Zip','Phone Number:','Secure Fax Number":','Patient ID/MRN:','Phone Number (required):','First Name:','Last Name:','Language Preference (optional):','DOB (mm/dd/yyyy):','Shipping Address:','Billing Address:','City, State, Zip:','City, State, Zip:','Policyholder Name:','Policyholder DOB:','Primary Insurance Carrier:','Claims Submission Address:','Subscriber ID/Policy Number:','Group Number:','Plan:','Prior-Authorization Code (if available):','Date:']
+other_texts = []
 
 for polygon in polygons:
-	print("LineText is: ",polygon[1])
+    if polygon[1] not in target_fields:
+        other_texts.append(polygon[1])
+# 	print("LineText is: ",polygon[1])
+
+
 
 # Display the image and overlay it with the extracted text.
 # image = Image.open(BytesIO(requests.get(image_url).content))
@@ -34,4 +39,6 @@ for polygon in polygons:
     ax.axes.add_patch(patch)
     # plt.text(vertices[0][0], vertices[0][1], text, fontsize=20, va="top")
 plt.show()
+
+print(other_texts)
 
