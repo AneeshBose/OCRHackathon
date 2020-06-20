170bc32293262b4d0d17b35d4d7c826701c65cf5
Udaya Kumar <udayakumar97@users.noreply.github.com>
2020-06-20 19:18:24 +0530

    Added save_pdf_to_image

--- utils.py
+++ utils.py
@@ -1,3 +1,12 @@
+from pdf2image import convert_from_path, convert_from_bytes
+from PIL import Image
+
+def save_pdf_to_image(pdf_path):
+    images = convert_from_path(pdf_path, dpi=120)
+    for i in range(len(images)):
+        img = images[i].resize((1240,1754), Image.ANTIALIAS)
+        img.save(pdf_path[:-2]+str(i)+'.jpg')
+        
 def get_offset(inputBox, outputBox):
     offset = [x1 - x2 for (x1, x2) in zip(outputBox, inputBox)]
     return offset
