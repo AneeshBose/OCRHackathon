d890759127a0b42902c108a3f50aea30ca86a789
Udaya Kumar <udayakumar97@gmail.com>
2020-06-26 03:16:56 +0530

    Page2 code added

--- Flask/parse_pdf.py
+++ Flask/parse_pdf.py
@@ -5,8 +5,10 @@ from PIL import Image
 import json
 import time
 from shapely.geometry import Polygon
-from utils import req_target_fields, req_other_texts, req_target_offsets, calculate_req_bounding_box, char2num, firebase_keys
 
+from utils import req_target_fields, req_other_texts, req_target_offsets, calculate_req_bounding_box
+from utils import info_target_fields, info_special_target_fields, info_other_texts, info_target_offsets, calculate_info_bounding_box
+from utils import char2num, firebase_keys
 
 def convert_pil_to_bytes(img):
 	buf = io.BytesIO()
@@ -33,7 +35,7 @@ def execute_api(byte_img):
 			response.headers["Operation-Location"], headers=headers)
 		analysis = response_final.json()
 
-		# print(json.dumps(analysis, indent=4))
+		#print(json.dumps(analysis, indent=4))
 
 		resp_data = json.dumps(analysis, indent=4)
 
@@ -48,7 +50,6 @@ def execute_api(byte_img):
 def first_page(img):
 	byte_img = convert_pil_to_bytes(img)
 	resp = execute_api(byte_img)
-	# print(resp)
 	target_bounding_boxes = {}
 	# Count each occurence of 'City, State, Zip'
 	csz_counter = 0
@@ -97,8 +98,6 @@ def first_page(img):
 			polygon_ptext = Polygon(target_bounding_boxes[target_field])
 
 			if polygon_ptext.contains(polygon_htext) == True:
-				# print(line['text'])
-				# print(target_field)
 
 				if 'Shipping Address' in target_field:
 					shipping_add.append((line['text'], [(line['boundingBox'][i], line['boundingBox'][i + 1]) for i in
@@ -244,14 +243,12 @@ def first_page(img):
 		field_mappings['DOB (mm/dd/yyyy)'] = field_mappings['DOB (mm/dd/yyyy)'].lstrip().rstrip()
 		del field_mappings['DOB (m']
 
-	cases = ["DOB","Number","NPI","Date"]
+	cases = ["DOB", "Number", "NPI", "Date"]
 	for key in field_mappings.keys():
-	    for case in cases:
-	        if case in key:
-	            field_mappings[key] = char2num(field_mappings[key])
-	            break
-
-
+		for case in cases:
+			if case in key:
+				field_mappings[key] = char2num(field_mappings[key])
+				break
 
 	for key in firebase_keys:
 		if key not in field_mappings:
@@ -259,22 +256,97 @@ def first_page(img):
 		else:
 			field_mappings[firebase_keys[key]] = field_mappings[key]
 			del field_mappings[key]
-	
+	return field_mappings
 
 
+def second_page(img):
+	byte_img = convert_pil_to_bytes(img)
+	resp = execute_api(byte_img)
+	target_bounding_boxes = {}
+
+	for index, line in enumerate(resp["analyzeResult"]["readResults"][0]["lines"]):
+		flag = 0
+		for field in info_target_fields:
+			if field.lower() in line['text'].lower() and field not in target_bounding_boxes:
+				bounding_box_points = calculate_info_bounding_box(line['boundingBox'], [0, -5, 0, -5, 0, 5, 0, 5])
+				target_bounding_boxes[field] = [(bounding_box_points[i], bounding_box_points[i + 1]) for i in
+												range(0, len(bounding_box_points), 2)]
+				flag = 1
+				break
+		if flag == 0:
+			for field in info_special_target_fields:
+				if field.lower() in line['text'].lower() and field not in target_bounding_boxes:
+					info_target_offsets[field][0] = 450
+					info_target_offsets[field][6] = 450
+					bounding_box_points = calculate_info_bounding_box(line['boundingBox'], info_target_offsets[field])
+					target_bounding_boxes[field] = [(bounding_box_points[i], bounding_box_points[i + 1]) for i in
+													range(0, len(bounding_box_points), 2)]
+
+	#print(target_bounding_boxes)
+	field_mappings = {}
+
+	for index, line in enumerate(resp["analyzeResult"]["readResults"][0]["lines"]):
+		bounding_coord = [(line['boundingBox'][i], line['boundingBox'][i + 1]) for i in
+						  range(0, len(line['boundingBox']), 2)]
+		# polygon_htext is response for each line in response
+		polygon_htext = Polygon(bounding_coord)
+		x1_coord = bounding_coord[0][0]
+		for target_field in target_bounding_boxes:
+			polygon_ptext = Polygon(target_bounding_boxes[target_field])
+
+			if polygon_ptext.contains(polygon_htext) == True:
+				if target_field not in field_mappings:
+					field_mappings[target_field] = [[line['text'], x1_coord]]
+				else:
+					field_mappings[target_field] += [[line['text'], x1_coord]]
+					field_mappings[target_field].sort(key=lambda x: x[1])
+
+				break
+			else:
+				pass
+
+	for key_field in field_mappings:
+		row = ''
+		for vals in field_mappings[key_field]:
+			row += vals[0] + ' '
+
+		if 'City, State, Zip' in key_field:
+			key_field_index = row.lower().find('City, State, Zip'.lower())
+			key_field_len = len(key_field)
+			str_key_field = row[key_field_index:key_field_index + key_field_len]
+			key_field_prep = row.replace(str_key_field, '')
+
+		else:
+			key_field_index = row.lower().find(key_field.lower())
+			key_field_len = len(key_field)
+			str_key_field = row[key_field_index:key_field_index + key_field_len]
+			key_field_prep = row.replace(str_key_field, '')
+
+		underscore_prep = key_field_prep.replace('_', '')
+		colon_prep = underscore_prep.replace(':', '')
+		dot_prep = colon_prep.lstrip('.')
+		dot_prep = dot_prep.rstrip('.')
+		final_prep = (dot_prep.lstrip()).rstrip()
+		field_mappings[key_field] = final_prep
+
+	cases = ["DOB", "Number", "NPI", "Date"]
+	for key in field_mappings.keys():
+		for case in cases:
+			if case in key:
+				field_mappings[key] = char2num(field_mappings[key])
+				break
 
 	return field_mappings
 
 
-
-def second_page(img):
-	return None
-
-
 def convert_pdf(pdf_path):
-	images = convert_from_bytes(open(r'./'+pdf_path,'rb').read())
+	images = convert_from_path(pdf_path, dpi=120)
 	for i in range(len(images)):
 		images[i] = images[i].resize((1240, 1754), Image.ANTIALIAS)
 	first_out = first_page(images[0])
-	# second_out = second_page(images[0])
+	second_out = second_page(images[1])
 	return first_out
+
+
+op = convert_pdf("C:\\Users\\rakes\Downloads\\test.pdf")
+print(op)
